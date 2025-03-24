6b10f88e44ef715ad1ae11f2e82be69b2664e762
EvertonLG <everton_gomes1997@hotmail.com>
2025-03-24 08:17:37 -0300

    Initial commit

---
+++ src/main/java/me/dio/santander/dev/week/_5/SantanderDevWeek2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander.dev.week._5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander/dev/week/_5/SantanderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander.dev.week._5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
